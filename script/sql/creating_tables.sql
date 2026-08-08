df9e94eb4bf9f03475f1b51ec2be12b3cebacf22
Preyash Srivastava <preyashsrivastava@gmail.com>
2026-08-08 12:44:37 +0530

    Update creating_tables.sql

--- script/sql/creating_tables.sql
+++ script/sql/creating_tables.sql
@@ -18,24 +18,3 @@ CREATE TABLE artist_streaming_info (
     solo_stream_pct FLOAT,
     collaborative_stream_pct FLOAT
 );
-GO
-
-IF OBJECT_ID('artist_streaming_info_clean', 'U') IS NOT NULL
-    DROP TABLE artist_streaming_info;
-GO
-CREATE TABLE artist_streaming_info_clean (
-    artist_name NVARCHAR(100),
-    sex NVARCHAR(20),
-    country_of_origin NVARCHAR(100),
-    primary_language NVARCHAR(50),
-    primary_genre NVARCHAR(100),
-    artist_type NVARCHAR(50),
-    debut_year INT,
-    total_streams_m FLOAT,
-    lead_streams_m FLOAT,
-    feature_streams_m FLOAT,
-    solo_streams_m FLOAT,
-    collaborative_streams_m FLOAT,
-    solo_stream_pct FLOAT,
-    collaborative_stream_pct FLOAT
-);
