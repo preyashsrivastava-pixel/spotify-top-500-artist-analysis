13c8b0ff2583822dd138ccbf6972e5dd02c47324
Preyash Srivastava <preyashsrivastava@gmail.com>
2026-08-08 14:24:44 +0530

    Update creating_table_artist_streaming_info_clean.sql

--- script/sql/creating_table_artist_streaming_info_clean.sql
+++ script/sql/creating_table_artist_streaming_info_clean.sql
@@ -12,9 +12,9 @@ SELECT
         CAST(total_streams_m AS REAL) AS total_streams,
         CAST(lead_streams_m AS REAL) AS lead_streams,
         CAST(feature_streams_m AS REAL) AS feature_streams,
-        CAST(solo_streams_m AS REAL) AS solo_streams,
-        CAST(collaborative_streams_m AS REAL) AS collaborative_streams,
+        CAST(solo_streams_m AS REAL) AS solo_streams,       
         CAST(solo_stream_pct AS REAL) AS pct_solo,
+        CAST(collaborative_streams_m AS REAL) AS collaborative_streams,
         CAST(collaborative_stream_pct AS REAL) AS pct_collab
   INTO artist_streaming_info_clean
   FROM artist_streaming_info;
